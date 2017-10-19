9c2513ddb0b1b9521577c3f4b58e8b4b92dfc3d5
kuminin <kumin.in@hotmail.com>
2017-10-19 13:30:13 -0700

    More Refactoring

--- src/main/java/com/gudden/maven/model/Normalizer.java
+++ src/main/java/com/gudden/maven/model/Normalizer.java
@@ -8,17 +8,9 @@ public class Normalizer {
 	public String normalize(String token) {
 		return token.replaceAll("^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$|\'", "");
 	}
-	
+
 	// ------------------------------------------------------------------------------------------------------
-	
-	/**
-	 * Splits words with hyphens in them and stores both the separated words, and
-	 * the original hyphenated word without the hyphens in between the words.
-	 * 
-	 * @param token
-	 *            The string
-	 * @return A set of all strings from the hyphenated word
-	 */
+
 	public Set<String> splitHypenWords(String token) {
 		Set<String> tokenSet = new HashSet<String>();
 		StringBuilder sb = new StringBuilder();
@@ -30,16 +22,9 @@ public class Normalizer {
 		tokenSet.add(sb.toString());
 		return tokenSet;
 	}
-	
+
 	// ------------------------------------------------------------------------------------------------------
 
-	/**
-	 * Stems a string using a stemming algorithm.
-	 * 
-	 * @param token
-	 *            The string
-	 * @return The stemmed string
-	 */
 	public String stem(String token) {
 		Stemmer stemmer = new Stemmer();
 		stemmer.add(token.toCharArray(), token.length());
